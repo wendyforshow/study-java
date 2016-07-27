d0dc5ab12df799bfc95563220418410c5e90a249
kiwiflydream <791650277@qq.com>
2016-07-27 15:35:31 +0800

    mybatis批量插入

--- study-spring-mybatis/src/main/java/xyz/mrwood/study/spring/mybatis/dao/OrderMapperExt.java
+++ study-spring-mybatis/src/main/java/xyz/mrwood/study/spring/mybatis/dao/OrderMapperExt.java
@@ -1,4 +1,10 @@
 package xyz.mrwood.study.spring.mybatis.dao;
 
+import java.util.HashMap;
+import java.util.List;
+
 public interface OrderMapperExt extends OrderMapper {
+
+    Integer addList(List<HashMap<String, Object>> list);
+
 }
--- study-spring-mybatis/src/test/java/xyz/mrwood/study/spring/mybatis/service/impl/OrderServiceTest.java
+++ study-spring-mybatis/src/test/java/xyz/mrwood/study/spring/mybatis/service/impl/OrderServiceTest.java
@@ -5,9 +5,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
+import xyz.mrwood.study.spring.mybatis.dao.OrderMapperExt;
 import xyz.mrwood.study.spring.mybatis.service.IOrderService;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * 项目：study
@@ -21,6 +25,8 @@ public class OrderServiceTest extends AbstractTestNGSpringContextTests {
 
     @Resource
     private IOrderService orderService;
+    @Resource
+    private OrderMapperExt orderMapperExt;
 
     @Test
     public void testGetByOrderId() throws Exception {
@@ -31,5 +37,26 @@ public class OrderServiceTest extends AbstractTestNGSpringContextTests {
 //        OrderService orderService = context.getBean("orderService", OrderService.class);
 
         System.err.println(orderService.getByOrderId("123456"));
+    }
+
+
+
+    @Test
+    public void testAddList(){
+
+        HashMap<String, Object> map1 = new HashMap<>();
+        map1.put("orderId", "11");
+        map1.put("userId", "2222");
+
+
+        HashMap<String, Object> map2 = new HashMap<>();
+        map2.put("orderId", "111");
+        map2.put("userId", "22122");
+
+        List<HashMap<String, Object>> list = new ArrayList<>();
+        list.add(map1);
+        list.add(map2);
+
+        orderMapperExt.addList(list);
     }
 }
